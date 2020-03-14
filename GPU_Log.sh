d69dfa2cd43cc32083f87183a04499f46757b8d3
hkisthebest <hkisthebest@users.noreply.github.com>
2020-03-14 15:14:55 +0800

    init

---
+++ GPU_Log.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+currentDate=$(date +%Y'-'%m'-'%d);
+read -p 'Where to(Leaving this blank will save the file under "~/logs/"): ' prefix;
+echo $prefix
+if [ -z "$prefix" ]
+then
+    echo "in if"
+    prefix="/Users/$USER/logs/";
+fi
+suffix="_GPU_LOG.log";
+echo $prefix;
+filename="$prefix$currentDate$suffix"
+echo $filename;
+touch $filename;
+read -p 'set logging interval: ' time
+while sleep $time;
+do
+date +%H':'%M':'%S >> $filename
+ioreg -l |grep \"PerformanceStatistics\" | cut -d '{' -f 2 | tr '|' ',' | tr -d '}' | tr ',' '\n'|grep 'Temp\|Fan\|Clock' >> $filename 
+echo '------------------------------' >> $filename 
+done
